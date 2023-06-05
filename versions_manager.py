ca54b41bc12fc544dd897f741fb4864d43697c10
adrienallouche <adrien.allouche@gmail.com>
2023-06-05 22:18:44 +0300

    versions_manager exercise

--- versions_manager.py
+++ versions_manager.py
@@ -1,57 +1,51 @@
 class VersionManager:
     def __init__(self, initial_version=''):
+        if initial_version:
+            version_parts = initial_version.split('.')
+            if len(version_parts) > 2:
+                version_parts = version_parts[:3]
 
-        self.version = initial_version
-        self.release_history = []
-        version_components = self.version.split(".")
-        for i in range(len(version_components)):
-            if not version_components[i].isdigit():
-                raise ValueError("Error occured while parsing version!")
-
-        self.release_history.append(self.version)
-
-        if len(version_components) == 1:
-            self.version = int(version_components[0])
-        elif len(version_components) == 2:
-            self.version = [
-                int(version_components[0]),
-                int(version_components[1])
-            ]
+            try:
+                self.major_version = int(version_parts[0])
+                self.minor_version = int(version_parts[1]) if len(version_parts) >= 1 else 0
+                self.patch_version = int(version_parts[2]) if len(version_parts) >= 2 else 0
+            except ValueError:
+                print("Error occurred while parsing version!")
         else:
-            self.version = [
-                int(version_components[0]),
-                int(version_components[1]),
-                int(version_components[2])
-            ]
+            self.major_version = 0
+            self.minor_version = 0
+            self.patch_version = 1
+
+        self.release_history = []
 
     def major(self, value=1):
-        self.version[0] += value
-        self.version[1] = 0
-        self.version[2] = 0
+        self.release_history.append((self.major_version, self.minor_version, self.patch_version))
+        self.major_version += value
+        self.minor_version = 0
+        self.patch_version = 0
         return self
 
     def minor(self, value=1):
-        self.version[1] += value
-        self.version[2] = 0
+        self.release_history.append((self.major_version, self.minor_version, self.patch_version))
+        self.minor_version += value
+        self.patch_version = 0
         return self
 
     def patch(self, value=1):
-        self.version[2] += value
+        self.release_history.append((self.major_version, self.minor_version, self.patch_version))
+        self.patch_version += value
         return self
 
     def rollback(self):
         # "1.1.1" -> '1.1.0'
-        if self.release_history is None:
+        if self.release_history:
+            self.major_version, self.minor_version, self.patch_version = self.release_history.pop()
+        else:
             raise Exception("Cannot rollback!")
-        self.version = self.release_history.pop()
         return self
 
     def release(self):
-
-        release = str(f'{self.major()}.{self.minor()}.{self.patch()}')
-
-        return release
+        return f"{self.major_version}.{self.minor_version}.{self.patch_version}"
 
 
-version = VersionManager("1").release()
-
+print(VersionManager().major().patch().rollback().release())
